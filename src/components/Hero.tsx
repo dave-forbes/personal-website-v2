75ae39e81ce0a45d1762d0dfb11b863d54b8400e
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-04 11:31:41 +0100

    update
    - modify links with animations and styles
    - use new blur background component
    - move iconfall to parent component, app as it logically makes more sense to put it there

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,88 +1,103 @@
 import { useState } from 'react';
-import IconFall from './IconFall';
+
 import { motion } from 'framer-motion';
 import { IoIosArrowRoundDown } from 'react-icons/io';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { memo } from 'react';
+import BlurBackground from './BlurBackground';
 
-const MemoizedIconFall = memo(IconFall);
+const toastVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 },
+};
 
 export default function Hero() {
-  const [copiedToClipboard, setCopiedToClipboard] = useState(false);
+  const [copiedToClipboard, setCopiedToClipboard] = useState('');
+
   return (
-    <div className="hero">
-      <MemoizedIconFall />
-      <div className="title blurry-element"></div>
-      <div className="title">
-        <motion.h1
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          Hi, I&apos;m Dave.
-        </motion.h1>
-        <motion.h2
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 1 }}
-        >
-          A web Developer based in London.
-        </motion.h2>
-        <div className="hero-buttons">
-          <motion.button
-            className="button"
+    <section className="hero">
+      <BlurBackground>
+        <div className="title">
+          <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 2 }}
+            transition={{ duration: 1 }}
           >
-            Tell me more <IoIosArrowRoundDown />{' '}
-          </motion.button>
-          <div className="icon-buttons">
+            Hi, I&apos;m Dave.
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            A web Developer based in London.
+          </motion.h2>
+          <div className="hero-buttons">
             <motion.a
-              href="https://github.com/dave-forbes"
-              target="_blank"
+              href="#about"
               className="button"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 2.2 }}
+              transition={{ duration: 1, delay: 2 }}
             >
-              <FaGithub />
+              Tell me more <IoIosArrowRoundDown />{' '}
             </motion.a>
-            <motion.a
-              href="https://www.linkedin.com/in/david-forbes-b372065b/"
-              target="_blank"
-              className="button"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 2.4 }}
-            >
-              <FaLinkedin />
-            </motion.a>
-            <motion.button
-              onClick={() => {
-                navigator.clipboard.writeText(
-                  'dave.forbes.dev@outlook.com'
-                );
-                setCopiedToClipboard(true);
-              }}
-              className="button"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 2.6 }}
-            >
-              <HiOutlineMail />
-            </motion.button>
-
-            <p
-              className="clipboard-confirm"
-              style={{ opacity: copiedToClipboard ? 1 : 0 }}
-            >
-              Copied to clipboard
-            </p>
+            <div className="icon-buttons">
+              <motion.a
+                href="https://github.com/dave-forbes"
+                target="_blank"
+                className="button"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 2.2 }}
+              >
+                <FaGithub />
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/david-forbes-b372065b/"
+                target="_blank"
+                className="button"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 2.4 }}
+              >
+                <FaLinkedin />
+              </motion.a>
+              <motion.button
+                onHoverStart={() => {
+                  setCopiedToClipboard('Copy to clipboard?');
+                }}
+                onHoverEnd={() => {
+                  setCopiedToClipboard('');
+                }}
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    'dave.forbes.dev@outlook.com'
+                  );
+                  setCopiedToClipboard('Copied!');
+                  setTimeout(() => {
+                    setCopiedToClipboard('');
+                  }, 1000);
+                }}
+                className="button"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 2.6 }}
+              >
+                <HiOutlineMail />
+                <motion.p
+                  className="toast"
+                  variants={toastVariants}
+                  initial="hidden"
+                  animate={copiedToClipboard ? 'visible' : 'hidden'}
+                >
+                  {copiedToClipboard}
+                </motion.p>
+              </motion.button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </BlurBackground>
+    </section>
   );
 }
