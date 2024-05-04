de5df93ec73348a67cdfe386b8f06165ba6ff0fe
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-04 12:59:38 +0100

    implement horizontal scroll

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,26 @@
+import { useRef } from 'react';
 import Project from './Project';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Projects() {
   const projects = [1, 2, 3, 4];
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({ target: targetRef });
+
+  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-95%']);
 
   return (
-    <section>
-      {projects.map(() => (
-        <Project />
-      ))}
+    <section ref={targetRef} className="projects-container">
+      <div className="projects-container-inner">
+        <motion.div
+          className="horizontal-scroll-container"
+          style={{ x }}
+        >
+          {projects.map(() => (
+            <Project />
+          ))}
+        </motion.div>
+      </div>
     </section>
   );
 }
