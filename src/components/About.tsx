b76e435eb6e5f264eb44fc6f191982cf15de13db
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-04 11:34:23 +0100

    create about component

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import BlurBackground from './BlurBackground';
+
+export default function About() {
+  return (
+    <section className="about" id="about">
+      <BlurBackground>
+        <p>
+          I am self taught developer, I'm passionate about building
+          things and learning new tools and technologies.
+        </p>
+        <p>
+          I'm looking for opportunities to work with other developers
+          and continuing my web developmnet journey.
+        </p>
+      </BlurBackground>
+    </section>
+  );
+}
