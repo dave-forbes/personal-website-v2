98038e1228cb80283e184105d2b829e5670dc476
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-04 11:32:34 +0100

    update
    - rename div classname
    - modify timings and delays etc. for animations, for aesthetic purposes

--- src/components/IconFlake.tsx
+++ src/components/IconFlake.tsx
@@ -6,13 +6,13 @@ interface IconFlakeProps {
 }
 
 export default function IconFlake({ Icon }: IconFlakeProps) {
-  const randomPos = Math.floor(Math.random() * 1000) - 1000 / 2;
+  const randomPos = Math.floor(Math.random() * 900) + 50;
 
-  const randomDuration = Math.floor(Math.random() * 10) + 5;
+  const randomDuration = Math.floor(Math.random() * 30) + 10;
 
-  const randomDelay = Math.floor(Math.random() * 10);
+  const randomDelay = Math.floor(Math.random() * 5);
 
-  const randomOpacity = Math.random() * (0.9 - 0.2) + 0.2;
+  const randomOpacity = Math.random() * (0.7 - 0.2) + 0.2;
 
   const randomSize = Math.floor(Math.random() * 30) + 20;
   const iconStyle = {
@@ -22,7 +22,7 @@ export default function IconFlake({ Icon }: IconFlakeProps) {
 
   return (
     <motion.div
-      className="rain-icon-container"
+      className="icon-flake-container"
       initial={{ y: 0, opacity: 0, x: randomPos }}
       animate={{
         y: '100vh',
@@ -30,7 +30,7 @@ export default function IconFlake({ Icon }: IconFlakeProps) {
       }}
       exit={{ opacity: 0 }}
       transition={{
-        ease: 'easeIn',
+        ease: 'easeInOut',
         duration: randomDuration,
         repeat: Infinity,
         delay: randomDelay,
