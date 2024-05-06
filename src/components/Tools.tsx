15ecd92bb62ab429def9caa824aad6a5ec24191f
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-06 12:44:44 +0100

    simplify tools section
    - remove tool component and use simple lists

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,19 +1,63 @@
-import Tool from './Tool';
+import BlurBackground from './BlurBackground';
 
 export default function Tools() {
   return (
     <section className="tools">
-      <Tool tool="React" size={100} />
-      <Tool tool="TypeScript" size={100} />
-      <Tool tool="JavaScript" size={100} />
-      <Tool tool="Visual Studio Code" size={100} />
-      <Tool tool="Chrome Dev Tools" size={100} />
-      <Tool tool="Git" size={100} />
-      <Tool tool="Github" size={100} />
-      <Tool tool="Netlify" size={100} />
-      <Tool tool="Railway" size={100} />
-      <Tool tool="HTML5" size={100} />
-      <Tool tool="HTML5" size={100} />
+      <BlurBackground>
+        <div className="grid">
+          <div>
+            <h2>Code Editor</h2>
+            <ul>
+              <li>Visual Studio Code</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Languages/Markup</h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>Pug</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Database</h2>
+            <ul>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Frameworks</h2>
+            <ul>
+              <li>React</li>
+              <li>NextJs</li>
+              <li>Vite</li>
+              <li>Express</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Testing</h2>
+            <ul>
+              <li>Jest</li>
+              <li>Vitest</li>
+              <li>SuperTest</li>
+              <li>React Testing Library</li>
+              <li>Chrome Dev Tools</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Other services</h2>
+            <ul>
+              <li>Github</li>
+              <li>Netlify</li>
+              <li>Railway</li>
+              <li>AWS</li>
+              <li>Webpack</li>
+            </ul>
+          </div>
+        </div>
+      </BlurBackground>
     </section>
   );
 }
