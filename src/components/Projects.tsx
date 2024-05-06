5efd2b979e73e4178e7a755de8f834bb91d4a75d
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-06 17:23:04 +0100

    modify
    - use window width to adjust scroll length
    - add section title
    - comment out project tags (temp)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,12 +8,19 @@ export default function Projects() {
   const targetRef = useRef<HTMLDivElement | null>(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-70%']);
+  const windowWidth = window.innerWidth;
+
+  const x = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ['1%', windowWidth > 650 ? '-70%' : '-75%']
+  );
 
   return (
     <section ref={targetRef} className="projects-container">
       <div className="projects-container-inner">
         <BlurBackground>
+          <h1 className="section-title">Recent Projects</h1>
           <motion.div
             className="horizontal-scroll-container"
             style={{ x }}
@@ -22,7 +29,7 @@ export default function Projects() {
               <Project
                 title={project.title}
                 image={project.img}
-                tags={project.tags}
+                // tags={project.tags}
                 description={project.description}
               />
             ))}
