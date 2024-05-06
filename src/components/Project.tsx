65f99caf84d9321a17fa6286eb1a841a12235dbe
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-06 17:22:16 +0100

    modify
    - add section title
    - comment out tags (temp)

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,29 +1,27 @@
-import ProjectTag from './ProjectTag';
+// import ProjectTag from './ProjectTag';
 import { FaRegFileCode } from 'react-icons/fa';
 import { LuExternalLink } from 'react-icons/lu';
 
 interface ProjectProps {
   image: string;
   title: string;
-  tags: string[];
   description: string;
 }
 
 export default function Project({
   image,
   title,
-  tags,
   description,
 }: ProjectProps) {
   return (
     <div className="project">
-      <h1>{title}</h1>
       <img src={image} />
-      <div className="project-tag-container">
+      <h1>{title}</h1>
+      {/* <div className="project-tag-container">
         {tags.map((tag) => (
           <ProjectTag content={tag} />
         ))}
-      </div>
+      </div> */}
       <p>{description}</p>
       <div className="project-link-container">
         <a href="" className="button light">
