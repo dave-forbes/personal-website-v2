8f0878a8b4fc705e7b2b2530907dcbdab855703a
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-06 12:43:56 +0100

    create projects section
    - with horizontal scroll

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,33 @@
 import { useRef } from 'react';
 import Project from './Project';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import BlurBackground from './BlurBackground';
+import { projects } from '../lib/data';
 
 export default function Projects() {
-  const projects = [1, 2, 3, 4];
   const targetRef = useRef<HTMLDivElement | null>(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-95%']);
+  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-70%']);
 
   return (
     <section ref={targetRef} className="projects-container">
       <div className="projects-container-inner">
-        <motion.div
-          className="horizontal-scroll-container"
-          style={{ x }}
-        >
-          {projects.map(() => (
-            <Project />
-          ))}
-        </motion.div>
+        <BlurBackground>
+          <motion.div
+            className="horizontal-scroll-container"
+            style={{ x }}
+          >
+            {projects.map((project) => (
+              <Project
+                title={project.title}
+                image={project.img}
+                tags={project.tags}
+                description={project.description}
+              />
+            ))}
+          </motion.div>
+        </BlurBackground>
       </div>
     </section>
   );
