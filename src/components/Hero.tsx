5475f8b9e5995aef5851df7eba609b0e8de9d176
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-10 18:26:05 +0100

    create a border class
    - and apply to hero button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
           <div className="hero-buttons">
             <motion.a
               href="#about"
-              className="button"
+              className="button border"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 1, delay: 2 }}
