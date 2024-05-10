123a90bdfbbf26db93a8071333f2be58e8b89651
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-10 18:21:15 +0100

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <section className="footer">
+      <a
+        href="https://github.com/dave-forbes?tab=repositories"
+        target="_blank"
+        className="button light border"
+      >
+        More projects on github
+      </a>
+      <a href="#hero" className="button dark">
+        Back to the top
+      </a>
+    </section>
+  );
+}
