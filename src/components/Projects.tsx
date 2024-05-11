084a9d2cd7d30f2dec02910905119440e2bd1655
David Forbes <6923901+dave-forbes@users.noreply.github.com>
2024-05-11 11:11:01 +0100

    replace blur background with div
    - component not needed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import Project from './Project';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import BlurBackground from './BlurBackground';
 import { projects } from '../lib/data';
 
 export default function Projects() {
@@ -19,7 +18,7 @@ export default function Projects() {
   return (
     <section ref={targetRef} className="projects-container">
       <div className="projects-container-inner">
-        <BlurBackground>
+        <div>
           <h1 className="section-title">Recent Projects</h1>
           <motion.div
             className="horizontal-scroll-container"
@@ -35,7 +34,7 @@ export default function Projects() {
               />
             ))}
           </motion.div>
-        </BlurBackground>
+        </div>
       </div>
     </section>
   );
